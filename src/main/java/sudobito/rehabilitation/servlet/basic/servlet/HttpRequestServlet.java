d27e9e15f14f13609f2ad6b0f3466d2e35097df1
sudobito <sdodkfu1@gmail.com>
2024-02-04 17:34:39 +0900

    fix: change directory

--- src/main/java/sudobito/rehabilitation/servlet/basic/servlet/HttpRequestServlet.java
+++ src/main/java/sudobito/rehabilitation/servlet/basic/servlet/HttpRequestServlet.java
@@ -1,4 +1,4 @@
-package sudobito.rehabilitation.servlet.basic;
+package sudobito.rehabilitation.servlet.basic.servlet;
 
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
