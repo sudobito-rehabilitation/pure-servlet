e8d2ca9258c67945f764fae6dcb3d6645b653553
odkfu <odkfu1@gmail.com>
2024-02-04 16:46:05 +0900

    feat : create [HttpRequestServlet] for checking information about HttpServletReqeust

---
+++ src/main/java/sudobito/rehabilitation/servlet/basic/HttpRequestServlet.java
@@ -0,0 +1,58 @@
+package sudobito.rehabilitation.servlet.basic;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "request-header-info", urlPatterns = "/request-header")
+public class HttpRequestServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) {
+        printStartLine(request);
+        printHeaders(request);
+        printEtc(request);
+    }
+
+    private void printStartLine(HttpServletRequest request) {
+        System.out.println("--- REQUEST-LINE - start ---");
+        System.out.println("request.getMethod() = " + request.getMethod());
+        System.out.println("request.getProtocol() = " + request.getProtocol());
+        System.out.println("request.getScheme() = " + request.getScheme());
+        System.out.println("request.getRequestURL() = " + request.getRequestURL());
+
+        System.out.println("request.getRequestURI() = " + request.getRequestURI());
+
+        System.out.println("request.getQueryString() = " + request.getQueryString());
+        System.out.println("request.isSecure() = " + request.isSecure()); // HTTPS 를 사용했는지
+        System.out.println("--- REQUEST-LINE - end ---");
+        System.out.println();
+    }
+
+    private void printHeaders(HttpServletRequest request) {
+        System.out.println("--- Headers - start ---");
+        request.getHeaderNames().asIterator()
+                .forEachRemaining(headerName -> System.out.println(headerName + ": "
+                        + request.getHeader(headerName)));
+        System.out.println("--- Headers - end ---");
+        System.out.println();
+
+
+    }
+
+    // 는 HTTP 메시지의 정보 외의 다른 정보들
+    private void printEtc(HttpServletRequest request) {
+        System.out.println("--- 기타 조회 start ---");
+        System.out.println("[Remote 정보]");
+        System.out.println("request.getRemoteHost() = " + request.getRemoteHost()); //
+        System.out.println("request.getRemoteAddr() = " + request.getRemoteAddr()); //
+        System.out.println("request.getRemotePort() = " + request.getRemotePort()); //
+        System.out.println();
+        System.out.println("[Local 정보]");
+        System.out.println("request.getLocalName() = " + request.getLocalName()); //
+        System.out.println("request.getLocalAddr() = " + request.getLocalAddr()); //
+        System.out.println("request.getLocalPort() = " + request.getLocalPort()); //
+        System.out.println("--- 기타 조회 end ---");
+        System.out.println();
+    }
+}
